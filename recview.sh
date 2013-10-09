f67f27c09d1560d9eb5332382f8bea1c9b5f6c26
Ethan House <ehouse@csh.rit.edu>
2013-10-09 00:43:09 -0700

    Cleared out API key
    I also regenerated it.

--- recview.sh
+++ recview.sh
@@ -1,6 +1,6 @@
 #!/usr/local/bin/bash
 curl https://www.cloudflare.com/api_json.html \
   -d 'a=rec_load_all' \
-  -d 'tkn=581311a1df396973926cbea44a67774dd33cb' \
-  -d 'email=ewhhouse@gmail.com' \
+  -d 'tkn=' \
+  -d 'email=' \
   -d 'z='
